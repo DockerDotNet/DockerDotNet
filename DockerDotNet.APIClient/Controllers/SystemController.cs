404a3ac36388fd0264e241bf8c32ac2f29ed1e3a
Psychac <Psychac@users.noreply.github.com>
2025-01-04 18:46:39 +0530

    Added Docker API Client

---
+++ DockerDotNet.APIClient/Controllers/SystemController.cs
@@ -0,0 +1,30 @@
+﻿using DockerDotNet.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerDotNet.APIClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+        DockerClient DockerClient { get; set; }
+
+        public SystemController()
+        {
+            DockerClient = new DockerClient();
+        }
+
+        [HttpGet]
+        [Route("version")]
+        public async Task<string> GetVersion()
+        {
+            HttpClient httpClient = DockerClient.GetDockerHttpClient();
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, new UriBuilder($"{httpClient.BaseAddress}version").Uri);
+            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(requestMessage);
+
+            return await httpResponseMessage.Content.ReadAsStringAsync();
+        }
+    }
+}
